0437f846bdea21bb963ba732e118c370914fe843
Ten <klav@live.de>
2018-10-09 11:02:35 +0200

    ok toll

---
+++ Aufgabe4 - ALTERNATIVE/soundPlayback.js
@@ -0,0 +1,34 @@
+var context = new AudioContext();
+
+var drumpads = document.getElementsByClassName("drumpad"),
+    soundBuffers = [];
+
+for (let i = 0; i < drumpads.length; i++) {
+	getData(i);
+	
+	drumpads[i].addEventListener("mousedown", function (e) {playSound(i)});
+}
+
+function getData(i){
+	var request = new XMLHttpRequest();
+	
+	// Befehl, Pfad, asynchron (wird im Hintergrund ausgeführt)
+	request.open("GET", "sounds/sound"+(i+1+".wav"), true; 
+	request.responseType = "arraybuffer";
+	request.onLoad(function(){
+		var undecodedAudio = request.response;
+		context.decodeAudioData(undecodedAudio, function(buffer){
+			soundBuffers[i] = buffer;
+		});
+		
+	});
+	
+	request.send();
+}
+
+function playSound(i) {
+	var sourceBuffer = context.createBufferSource();
+	sourceBuffer.buffer = soundBuffers[i];
+	sourceBuffer.connect(context.destination);
+    soundBuffer.start(0);
+}
